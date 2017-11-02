ec78b52e085588d37111f9c86631719aec83b17f
mgabere <mgabere@gmail.com>
2017-11-02 09:00:40 +0300

    Update QC_T1.R

--- scripts/Quality_control/QC_T1.R
+++ scripts/Quality_control/QC_T1.R
@@ -1,4 +1,13 @@
 #Load the necessary libraries
+library(affy)
+library(affyQCReport)
+
+dat <- ReadAffy()
+eset <- rma(dat)
+
+QCReport(dat, "QC_report.pdf")
+
+
 #QC Analysis using arrayQualityMetrics software
 require(arrayQualityMetrics)
-arrayQualityMetrics(expressionset = eset, outdir = "Report_for_Heat_Stroke_Cels_T1", force = TRUE)
+arrayQualityMetrics(expressionset = eset, outdir = "Report_for_HeatStress_T1_30OCT17", force = TRUE)
