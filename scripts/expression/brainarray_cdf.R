309a4ac9eee2d978e47c7f4fdea9dc2f6f5bd978
mgabere <mgabere@gmail.com>
2017-12-24 10:00:40 +0300

    Update brainarray_cdf.R

--- scripts/expression/brainarray_cdf.R
+++ scripts/expression/brainarray_cdf.R
@@ -1,1 +1,34 @@
+library(affy)
+library(hgu133plus2hsentrezgcdf) # cdfname = "HGU133Plus2_Hs_ENTREZG"
+library(hgu133plus2hsentrezgprobe)
+library(hgu133plus2hsentrezg.db)
+library(genefilter)
 
+
+#Set CDF to use
+cdf = "HGU133Plus2_Hs_ENTREZG"
+
+#Read in the raw data from specified Directory of CEL files
+dat <- ReadAffy(verbose=TRUE, cdfname=cdf)
+celfile_rma <- rma(dat)
+
+#Get the important stuff out of the data - the expression estimates for each array
+eset <- exprs(celfile_rma)
+
+#Format values to 5 decimal places
+eset = format(eset, digits=5)
+
+#Extract probe ids, entrez symbols, and entrez ids
+probes=row.names(eset)
+
+#Entrez_IDs = unlist(mget(probes,hgu133plus2Entrez_IDs, ifnotfound=NA))
+Symbols = unlist(mget(probes, hgu133plus2hsentrezgSYMBOL, ifnotfound=NA))
+#Genename = unlist(mget(probes, hgu133plus2GENENAME, ifnotfound=NA))
+
+#Combine gene annotations with raw data
+eset=cbind(probes,Symbols, eset)
+ 
+
+#Write RMA-normalized, mapped data to file
+#write.table(rma, file = "expression.txt", quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+write.table(eset, file = "heat_stress_cdf_v19.csv", quote = FALSE, sep = ",", row.names = FALSE, col.names = TRUE)
